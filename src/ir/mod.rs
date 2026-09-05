ee22fa33e9391b503844c760090cf50375b2cada
2vex <2nosym@gmail.com>
2026-09-05 16:39:49 -0500

    feat: expose typed IR operands

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,7 +1,9 @@
 mod block;
 mod inst;
+mod operand;
 mod value;
 
 pub use block::Block;
 pub use inst::{IRInst, Opcode};
+pub use operand::{GuestReg, Operand, RegKind, RegWidth};
 pub use value::Value;
