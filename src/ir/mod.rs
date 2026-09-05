6f58d7754b38d4e1d1dc00a34b58336547ab5295
2vex <2nosym@gmail.com>
2026-09-05 16:43:45 -0500

    fix: re-export IR flags and shift operands

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -4,6 +4,6 @@ mod operand;
 mod value;
 
 pub use block::Block;
-pub use inst::{IRInst, Opcode};
-pub use operand::{GuestReg, Operand, RegKind, RegWidth};
+pub use inst::{IRInst, Opcode, FLAG_WRITES_NZCV};
+pub use operand::{GuestReg, Operand, RegKind, RegWidth, ShiftKind};
 pub use value::Value;
