4e0cca2e791227bd8b22c69ca9a3251a2b33641a
2vex <2nosym@gmail.com>
2026-09-05 17:13:47 -0500

    feat: expose guest PC offset for branch lowering

--- src/runtime/state.rs
+++ src/runtime/state.rs
@@ -14,26 +14,17 @@ pub struct GuestState {
 
 impl GuestState {
     #[inline]
-    pub const fn new() -> Self {
-        Self { gpr: [0; 31], sp: 0, nzcv: 0, _pad: 0, pc: 0 }
-    }
-
+    pub const fn new() -> Self { Self { gpr: [0; 31], sp: 0, nzcv: 0, _pad: 0, pc: 0 } }
     #[inline]
-    pub fn read_x(&self, reg: u8) -> u64 {
-        match reg { 0..=30 => self.gpr[reg as usize], 31 => 0, _ => unreachable!() }
-    }
-
+    pub fn read_x(&self, reg: u8) -> u64 { match reg { 0..=30 => self.gpr[reg as usize], 31 => 0, _ => unreachable!() } }
     #[inline]
-    pub fn write_x(&mut self, reg: u8, value: u64) {
-        if reg < 31 { self.gpr[reg as usize] = value; }
-    }
-
+    pub fn write_x(&mut self, reg: u8, value: u64) { if reg < 31 { self.gpr[reg as usize] = value; } }
     #[inline]
     pub fn read_sp(&self) -> u64 { self.sp }
-
     #[inline]
     pub fn write_sp(&mut self, value: u64) { self.sp = value; }
 }
 
 pub const GPR_BASE: i32 = offset_of!(GuestState, gpr) as i32;
 pub const SP_OFFSET: i32 = offset_of!(GuestState, sp) as i32;
+pub const PC_OFFSET: i32 = offset_of!(GuestState, pc) as i32;
