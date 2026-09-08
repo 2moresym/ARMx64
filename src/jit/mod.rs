f1bfebf5d96ef5bf7b39b34c965611fdc397ff85
2vex <2nosym@gmail.com>
2026-09-08 00:13:09 -0500

    feat(jit): expose IR optimizer

--- src/jit/mod.rs
+++ src/jit/mod.rs
@@ -1,7 +1,9 @@
 mod cache;
 mod compiler;
 mod hotness;
+mod optimizer;
 
 pub use cache::CodeCache;
 pub use compiler::{BackgroundCompiler, CompileRequest, CompiledBlock};
 pub use hotness::Hotness;
+pub use optimizer::Optimizer;
